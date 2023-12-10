659f4a1b20ee13e200f0e28068396219814c9b49
liubufan1998 <ly17369279121@163.com>
2023-12-10 19:07:05 +0800

    001

---
+++ build/CMakeFiles/lytest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lytest.dir/MSB.cpp.obj"
+  "CMakeFiles/lytest.dir/MSB.cpp.obj.d"
+  "liblytest.dll.a"
+  "lytest.exe"
+  "lytest.exe.manifest"
+  "lytest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lytest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/lytest.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for lytest.
+# This may be replaced when dependencies are built.
